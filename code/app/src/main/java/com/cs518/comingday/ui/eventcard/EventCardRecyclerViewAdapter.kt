1c145ab10239b72059c645d75a268c677a252c90
Yaoxuan Wei <justin13wyx@gmail.com>
2020-11-09 10:37:47 -0600

    [新增] 事件卡片的Adapter, databinding完成 [弃用] header和header_holder的xml, 使用弹窗代替header_holder, 取消置顶功能 因此不再需要header

---
+++ code/app/src/main/java/com/cs518/comingday/ui/eventcard/EventCardRecyclerViewAdapter.kt
@@ -0,0 +1,56 @@
+package com.cs518.comingday.ui.eventcard
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.cs518.comingday.database.Event
+import com.cs518.comingday.databinding.ListItemEventCardBinding
+
+class EventCardRecyclerViewAdapter(val cardClickListener: EventCardClickListener) : ListAdapter<Event, EventCardRecyclerViewAdapter.ViewHolder>(EventDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, cardClickListener)
+    }
+
+
+    class ViewHolder private constructor(val binding: ListItemEventCardBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Event, cardClickListener: EventCardClickListener) {
+            binding.event = item
+            binding.clickListener = cardClickListener
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemEventCardBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+
+    }
+
+}
+
+class EventDiffCallback : DiffUtil.ItemCallback<Event>() {
+    override fun areItemsTheSame(oldItem: Event, newItem: Event): Boolean {
+        return oldItem.eventId == newItem.eventId
+    }
+
+    override fun areContentsTheSame(oldItem: Event, newItem: Event): Boolean {
+        return oldItem == newItem
+    }
+
+}
+
+class EventCardClickListener(val clickListener: (eventId: Long) -> Unit) {
+    fun onClick(event: Event) = clickListener(event.eventId)
+}
